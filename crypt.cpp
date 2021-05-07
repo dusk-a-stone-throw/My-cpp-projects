61847b4ad399314a713560517f4b272a018653c1
REEPSIW <mihailzomitev@gmail.com>
2021-05-07 11:00:24 +0300

    Caesar's code

--- crypt.cpp
+++ crypt.cpp
@@ -1,7 +1,23 @@
+#include <clocale>
 #include <iostream>
+#include <string>
 using std::cout;
 using std::cin;
 using std::endl;
+using std::string;
+// Прописные буквы в таблице с 97 по 122
 int main() {
+    setlocale(LC_ALL, "ru");
+    int key;
+    char word;
+    cin >> key;
+    cin >> word;
+    // cout << (int)word << endl;
+    if(int(word) <= (122 - key)) {
+        cout << char(int(word) + key) << endl;
+    }
+    else {
+        cout << "fuck you" << endl;
+    }
     return 0;
 }
