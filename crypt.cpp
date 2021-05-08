7df243c68d85b8002f81ccbe3a02204c7b6e8fa0
REEPSIW <mihailzomitev@gmail.com>
2021-05-08 20:01:28 +0300

    Ceasar cipher

--- crypt.cpp
+++ crypt.cpp
@@ -14,10 +14,7 @@ void encrypt(string word, int key) { // Шифрование
             cout << word[i];
         }
         else if(int(word[i]) >= 97 && int(word[i]) <= 122) { // Если буква прописная
-            if(int(word[i]) == 32) {
-                cout << " ";
-            }
-            else if(int(word[i]) <= (122 - key)) { // если не надо идти в начало
+            if(int(word[i]) <= (122 - key)) { // если не надо идти в начало
                 cout << char(int(word[i]) + key);
             }
             else {
@@ -25,15 +22,11 @@ void encrypt(string word, int key) { // Шифрование
             }
         }
         else { // Если заглавная
-            if (int(word[i]) <= (90 - key)) {
+            if(int(word[i]) <= (90 - key)) { // если не надо идти в начало
                 cout << char(int(word[i]) + key);
-            }
-            else if(int(word[i]) == 32) { // 32 - это пробел
-                cout << " ";
             }
             else {
                 cout << char ((key - (90 - int(word[i])) - 1) + 65);
-                // a уже считается за первый символ, поэтому - 1
             }
         }
     }
@@ -41,21 +34,60 @@ cout << endl;
 }
 void decrypt(string word, int key) {
     for(int i = 0; i < word.size(); i++) {
-        if(int(word[i]) == 32) {
-            cout << " ";
-        }
-
-        else if(int(word[i]) < 65 || int(word[i]) > 122) {
+        if(int(word[i]) < 65 || int(word[i]) > 122) {
             cout << word[i];
         }
-
-        else {
-            cout << char(int(word[i]) - key);
+        else if(int(word[i]) >= 97 && int(word[i]) <= 122) { // Если буква прописная
+            if(int(word[i]) <= 122 - (26 - key)) { // если не надо идти в начало
+                cout << char(int(word[i]) + (26 - key));
+            }
+            else {
+                cout << char(int(word[i]) -  key);
+            }
+            // else {
+            //     cout << char ((key - (122 - int(word[i])) - 1) + 97); // a уже считается за первый символ, поэтому - 1
+            // }
+        }
+        else { // Если заглавная
+            if(int(word[i]) <= 90 - (26 - key)) { // если не надо идти в начало
+                cout << char(int(word[i]) + (26 - key));
+            }
+            else {
+                cout << char(int(word[i]) - key);
+            }
         }
     }
     cout << endl;
 }
-
+void bruteForce(string word) { // Перебираем фразу всеми ключами, от 1 до 25 (0 и 26 не влияют на текст)
+    for(int j = 1; j < 26; j++) {
+        for(int i = 0; i < word.size(); i++) {
+            if(int(word[i]) < 65 || int(word[i]) > 122) {
+            cout << word[i];
+            }
+            else if(int(word[i]) >= 97 && int(word[i]) <= 122) { // Если буква прописная
+                if(int(word[i]) <= 122 - (26 - j)) { // если не надо идти в начало
+                    cout << char(int(word[i]) + (26 - j));
+                }
+                else {
+                    cout << char(int(word[i]) - j);
+                }
+                // else {
+                //     cout << char ((key - (122 - int(word[i])) - 1) + 97); // a уже считается за первый символ, поэтому - 1
+                // }
+            }
+            else { // Если заглавная
+                if(int(word[i]) <= 90 - (26 - j)) { // если не надо идти в начало
+                    cout << char(int(word[i]) + (26 - j));
+                }
+                else {
+                    cout << char(int(word[i]) - j);
+                }
+            } 
+        }
+        cout << endl << endl;
+    }
+}
 int main() {
     setlocale(LC_ALL, "ru");
     cout << "Welcome to Ceasar Cipher decrypter/encrypter." << endl;
@@ -72,7 +104,7 @@ int main() {
             cin.ignore(INT32_MAX, '\n');
             cout << "Enter a phrase: ";
             std::getline(cin, word);
-            cout << "Enter 1 to ENCRYPT your phrase, 2 to DECRYPT, 3 to EXIT: ";
+            cout << "Enter 1 to ENCRYPT your phrase, 2 to DECRYPT, 3 to EXIT, 4 to MATCH your phrase with BRUTE FORCE METHOD: ";
             cin >> choise;
             switch (choise) {
                 case '1': {
@@ -86,6 +118,10 @@ int main() {
                 case '3': {
                     cout << "Thanks for using" << endl;
                     exit(0);
+                    break;
+                }
+                case '4': {
+                    bruteForce(word);
                     break;
                 }
                 default: {
