062d000b201be8889646cc9b4c45d94639cf2803
REEPSIW <mihailzomitev@gmail.com>
2021-05-31 10:34:08 +0300

    RUS-ENG layout changer

--- layoutChanger/layoutChanger.cpp
+++ layoutChanger/layoutChanger.cpp
@@ -1,11 +1,12 @@
-#include <clocale>
+#include <locale>
 #include <iostream>
 #include "parsing.cpp"
 using std::cout;
 using std::endl;
 using std::string;
 int main(int argc, char *argv[]) {
-    setlocale(LC_ALL, "ru");
-    cout << parsing("zzz") << endl;
+    setlocale(LC_CTYPE, "");
+    parsing(argv[1]);
+    cout << endl;
     return 0;
 }
