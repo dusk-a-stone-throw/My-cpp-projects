550eb10f6de430363b54c4540ca41c1bbcc8066b
REEPSIW <mihailzomitev@gmail.com>
2021-06-02 12:35:59 +0300

    file Reorganization

--- project1/src/addh.h
+++
@@ -1,4 +0,0 @@
-#ifndef ADDH
-#define ADDH
-int add(int x, int y);
-#endif
--- project1/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "addh.h"
-using namespace std;
-// int add(int x, int y);
-int main() {
-    cout << add(3, 5) << endl;
-    return 0;
-}
