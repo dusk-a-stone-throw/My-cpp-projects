cb93e9dc3e3b3e0645b8e20beb03a5b4def31de0
REEPSIW <mihailzomitev@gmail.com>
2021-06-11 15:47:50 +0300

    Console minesweeper game

---
+++ Minesweeper/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    for(int i = 0; i < 5; i++) {
+        for(int j = 0; j < 5; j++) {
+            std::cout << " ▅";
+        }
+        std::cout << std::endl;
+    }
+}
