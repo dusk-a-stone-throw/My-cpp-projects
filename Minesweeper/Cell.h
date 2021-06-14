2e12ae2353971c8a1368718e63c99eb7ac1a12fe
REEPSIW <mihailzomitev@gmail.com>
2021-06-14 09:57:22 +0300

    .

--- Minesweeper/Cell.h
+++
@@ -1,9 +0,0 @@
-#include <string>
-class Cell {
-private:
-    std::string type;
-public:
-    Cell();
-    void SetType(std::string type);
-    std::string GetType();
-};
--- Minesweeper/DrawField.h
+++
@@ -1,4 +0,0 @@
-#ifndef DRAWFIELD_H
-#define DRAWFIELD_H
-void drawField(Cell field[][], int height, int width);
-#endif
