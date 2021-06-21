67ea5ab867fd948b147a290aa7fc97af146cc9f2
REEPSIW <mihailzomitev@gmail.com>
2021-06-21 20:18:11 +0300

    Console 2048 game

--- 2048/DrawGrid.cpp
+++ 2048/DrawGrid.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 void DrawGrid(Tile grid[4][4]) {
     for(int i = 0; i < 4; i++) {
-        // std::cout << "———————— ————————— ————————— —————————" << std::endl;
+        std::cout << "———————— ————————— ————————— —————————" << std::endl;
         for(int j = 0; j < 4; j++) {
             std::cout << "| " << grid[i][j].GetValue() << " |";
         }
