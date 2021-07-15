15e7ada562448067f98d4ac9e791ebf51e79a5bd
REEPSIW <mihailzomitev@gmail.com>
2021-07-15 06:35:54 +0000

    Console Tic Tac Toe game

--- Tic-Tac-Toe/drawBoard.cpp
+++ Tic-Tac-Toe/drawBoard.cpp
@@ -1,10 +1,14 @@
-#include "figure.h"
 #include <iostream>
-void drawBoard(Figure board[3][3]) {
-    for(int i = 0; i < 3; i++) {
+
+#include "figure.h"
+void drawBoard(Figure board[3][3])
+{
+    for (int i = 0; i < 3; i++)
+    {
         std::cout << "—————————————" << std::endl;
         std::cout << "| ";
-        for(int j = 0; j < 3; j++) {
+        for (int j = 0; j < 3; j++)
+        {
             std::cout << board[i][j].GetType() << " | ";
         }
         std::cout << std::endl;
